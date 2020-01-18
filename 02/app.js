7b290560538f6e60572b83405f577e8f3f952ae1
Michal Robert Adamczyk <michal.robert.adamczyk@gmail.com>
2020-01-18 10:09:34 +0100

    rozpisane zadania i problem z funkcją do walidacji e-maila

--- 02/app.js
+++ 02/app.js
@@ -1,6 +1,21 @@
-// 1. odwołać się do poszczególnych pól formularza
-// 2. ustawić nasłuchiwanie keyup na każde pole 
-3. 
+// 1. Odwołać się do wszystkich pól formularza i zatrzymać domyślną akcję
+console.log('task 1 done');
+
+// 2. sprawdzić czy dwa hasła są takie same i mają ponad 6 znaków
+// console.log('task 2 done');
+
+// 3. sprawdzić czy zostal poprawnie wprowadzony email
+// console.log('task 2 done');
+
+// rozumiem co robi ta funkcja ale najwyraźniej umyka mi coś w samej zasadzie działania funkcji ogólnie
+
+
+
+
+// function validateEmail(elementValue){      
+//   const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+//   return emailPattern.test(elementValue); 
+// } 
 
 
 const formEl = document.querySelector('form');
@@ -10,14 +25,20 @@ formEl.addEventListener('submit',function(e){
 
     const email = e.target.elements.login;
     console.log(email.value);
-
-    const elementValue = email.value
-
-    function validateEmail(elementValue){      
-       const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-       return emailPattern.test(elementValue); 
-     }
-
     
 });
 
+
+
+
+
+
+
+// 4. warunek if wszystko ok --> console.log('done');
+//  if (walidacja e-maila oreaz obydwa paswordy takie same){
+//   console.log('done');
+// };
+
+// 5. warunek if not --> kolor fontu dla lebela z błędnie wprowadzonymi danymi zmienić na czerwono
+// zrobić tablicę z błędnie wprowadzonymi danymie `const errors = []` i dodać do niej selektors elementu z błędnymi danymi
+// pętla (`for` lub `forEach`), która zaznaczy elementy z błędem
