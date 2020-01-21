37ed14ed11d011e315592cc0d2c7a6f8ce1c53db
Michal Robert Adamczyk <michal.robert.adamczyk@gmail.com>
2020-01-21 16:06:04 +0100

    atribut style

--- 02/app.js
+++ 02/app.js
@@ -1,77 +1,86 @@
+// jaki selektor byłby lepszy?
+const formLogin= document.querySelector('[for="formLogin"]');
+
+console.log(formLogin);
+
+console.log(formLogin.hasAttribute("for"));
+
+formLogin.setAttribute('style', 'color:red');
 
 
+// const formEl = document.querySelector('form');
+
+// formEl.noValidate=true;
+
+// function validateEmail(elementValue){      
+//     const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+//     return emailPattern.test(elementValue); 
+//   };
+
+// function validatePasword(elementValue){      
+//     const passwordPattern = /^.{6,}$/;
+//     return passwordPattern.test(elementValue); 
+// };
+
+// //zrobić żeby ta funkcja zmieniała kolor elementu na czerwony 
+// function colourChanger(element){
+//     console.log(element);
+// };
+// formEl.addEventListener('submit',function(e){
+//     e.preventDefault();
+
+//     const errors = [];
+
+//     const email = e.target.elements.login;
+//     const emailValue = e.target.elements.login.value;
+      
+//     if (validateEmail(emailValue)){
+//         }
+//     else{
+//         errors.push('zły emial');
+// // tutaj testowo zrobić tak żeby się kolor zmieniał, potem to samo zastosować w pętli
+//         // email.setAttribute('style', 'color:#FF0000');
+
+//     };
 
 
-const formEl = document.querySelector('form');
-
-formEl.noValidate=true;
-
-function validateEmail(elementValue){      
-    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    return emailPattern.test(elementValue); 
-  };
-
-  function validatePasword(elementValue){      
-    const passwordPattern = /^.{6,}$/;
-    return passwordPattern.test(elementValue); 
-  };
-formEl.addEventListener('submit',function(e){
-    e.preventDefault();
-
-    const errors = [];
-    // console.log(errors);
-
-    const email = e.target.elements.login.value;
-    // console.log(email);
-    // console.log(validateEmail(email))
+//     const forPass1 = e.target.elements.pass1.value;
+//     if (validatePasword(forPass1)){
+//             }
+//     else{
+//         errors.push('hasło nie ma 6 znaków');
+//     };
     
-    if (validateEmail(email)){
-        // console.log(validateEmail(email));
-    }
-    else{
-        errors.push('zły emial');
-    };
-
-
-    const forPass1 = e.target.elements.pass1.value;
-    if (validatePasword(forPass1)){
-        // console.log(validateEmail(email));
-    }
-    else{
-        errors.push('hasło nie ma 6 znaków');
-    };
-    // console.log(forPass1);
-    // console.log(validatePasword(forPass1));
-
-    const forPass2 = e.target.elements.pass2.value;
-    // console.log(forPass2);
-    if (forPass1==forPass2){
-        // console.log('same');
-    }
-    else{
-        errors.push('hasła nie są takie same');
-        // console.log('different');
-    };
+//     const forPass2 = e.target.elements.pass2.value;
+//         if (forPass1==forPass2){
+//             }
+//     else{
+//         errors.push('hasła nie są takie same');
+//           };
         
     
-    const accept = e.target.elements.accept;
-    if (accept.checked){
-        // console.log('checked');
-    }
-    else{
-        errors.push('regulamin nie został zaakceptowany');
-        // console.log('not checked');
-    }
-    console.log(errors);
+//     const accept = e.target.elements.accept;
+//     if (accept.checked){
+//             }
+//     else{
+//         errors.push('regulamin nie został zaakceptowany');
+//             }
+  
+    
+    
+//     if (errors.length==0){
+//         console.log("done");
+//     }
+//     else{
         
-});
+//         console.log(errors);
+//         errors.forEach(colourChanger);
+//     }
+        
+// });
 
 
 
+// const head = document.querySelector('head');
 
-
-
-
-// 5. warunek if not --> kolor fontu dla lebela z błędnie wprowadzonymi danymi zmienić na czerwono
-// zrobić tablicę z błędnie wprowadzonymi danymie `const errors = []` i dodać do niej selektors elementu z błędnymi danymi
-// pętla (`for` lub `forEach`), która zaznaczy elementy z błędem
+// console.log(head);
