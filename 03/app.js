319956c7aebcdfa114c6c0f257b6dde2174a0663
Michał Adamczyk <michal.robert.adamczyk@gmail.com>
2020-01-31 13:59:45 +0100

    feature - loading and showing every picture with header, footer and classes

--- 03/app.js
+++ 03/app.js
@@ -1,72 +1,48 @@
-// wyświetlenie w `.images-list` 
-
-
-// wszystkich wybranych (event `change`) 
-// w polu `<input/>` 
-// plików graficznych (`.type.includes('image')`).
-
-// Należy wykorzystać strukturę z elementu `.images-list__item--prototype` 
-// (można go sklonować) przy czym należy zuwaćyć, 
-// że element o tej klasie jest nie widoczny dla użytkownika.
-
-// Zwróć uwagę również, że ten `input` typu `file` 
-// pozwala wybierac więcej niż jeden plik dlatego należy użyć pętli `for` 
-// przy prezentowaniu wybranych przez użytkownika plików.
-
-// Nie zapomnij o wyświetleniu nazwy i rozmiaru pliku 
-// w MB z dwoma miejscami po przecinku.
-
-
-// const fileEl = document.querySelector('input');
-// console.log(fileEl);
-
-
-// fileEl.addEventListener('change', showInfoFile);
-
-
-// function showInfoFile() {
-//     const fileList = document.querySelectorAll('input');
-//     console.log(fileList);
-//     const file = e.target.files[0];
-//     console.log(
-//         file.name,
-//         file.size,
-//         file.type,
-//     );
-// };
-
-
 const input = document.querySelector('input[type="file"]')
 const imagesList = document.querySelector(`.images-list`);
 
-// czy tutaj uzywam funkcji readFile, ktrą definiuję linijkę niżej
+// czy tutaj uzywam funkcji readFile, ktrą definiuję linijkę niżej ??
 input.addEventListener('change', readFile);
 
 function readFile(e) {
-    const file = e.target.files[0];
 
-    // includes to jest właściwość czy metoda czy co to jest?    
-    if (file && file.type.includes('image')) {
-        const reader = new FileReader();
-        reader.onload = function(readerEvent) {
-            const newImg = document.createElement('img');
-            // co to jest .src ??
-            // co to jest .target.result ??
-            newImg.src = readerEvent.target.result;
-            imagesList.appendChild(newImg);
+
+    const fileList = e.target.files;
+
+    for (let i = 0; i < fileList.length; i++) {
+        console.log(fileList[i]);
+        const file = fileList[i];
+        console.log(file)
+            // includes to jest właściwość czy metoda czy co to jest?   
+        if (file && file.type.includes('image')) {
+            const reader = new FileReader();
+            reader.onload = function(readerEvent) {
+                const newImg = document.createElement('img');
+                // co to jest .src ??
+                // co to jest .target.result ??
+                newImg.src = readerEvent.target.result;
+                const newHeader = document.createElement('header');
+                const newLi = document.createElement('li');
+                const newFooter = document.createElement('footer');
+                imagesList.appendChild(newLi);
+                newLi.appendChild(newHeader);
+                newLi.appendChild(newImg);
+                newLi.appendChild(newFooter);
+                newLi.className = "images-list__item";
+                newHeader.className = "images-list__item-name";
+                newImg.className = "images-list__item-img";
+                newFooter.className = "images-list__item-size";
+            };
+            reader.readAsDataURL(file);
         };
-        reader.readAsDataURL(file);
-    };
+    }
+
+
+
 
 }
 
-// {
-//     console.log(input.files);
+
 //     co to jest za konstrukcja const cośtam = new costam() ??
 //     czy reader to normalna zmienna, do której przypisuję funkcję FilrReader() ??
 //     const reader = new FileReader();
-//     reader.onload = function(){
-
-//     }
-
-// }, false);
