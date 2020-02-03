e4c72230bdc3d00d28076f2fb1fa6923a63ff444
Michał Adamczyk <michal.robert.adamczyk@gmail.com>
2020-02-03 14:47:33 +0100

    zadanie czwarte, w komentarzach etapy do zrobienia, na razie próbuję rozgryźć jak działają funkcje

--- 04/app.js
+++ 04/app.js
@@ -1,10 +1,28 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
+    // 3. tworzę potrzebne mi zmienne
+    const rangeEl = document.querySelector('[type="range"]');
+    const colorEl = document.querySelector('[type="color"]');
+    rangeEl.setAttribute('min', '0,0');
+    rangeEl.setAttribute('max', '1,0');
+    // rangeEl.setAttribute('step', '1');
+
+    // ?? dalczego jak ustawiam value na 1 albo 0 to tam się znajduje suwak, a jak na inną wartość, to w połowie??
+    rangeEl.setAttribute('value', '0,1');
+    console.log(rangeEl);
+    console.log(colorEl);
+    // 3. tworzę potrzebne mi zmienne
+
+    // 4. tworzę nasłuchwianie dla zmiennych 
+
+    // 4. tworzę nasłuchwianie dla zmiennych 
+
     const boxElement = document.querySelector('.box');
     setBoxShadow(boxElement, '#000000');
 }
 
+// Ta funkcja ustawia ??
 function setBoxShadow(element, colorInHex, opacity = 1) {
     const colorInRGBA = `rgba(
         ${getChannelColor(colorInHex, 'red')}, 
@@ -19,7 +37,7 @@ function setBoxShadow(element, colorInHex, opacity = 1) {
 
 function getChannelColor(colorInHex, channelName) {
     let start;
-    switch(channelName) {
+    switch (channelName) {
         case 'red':
             start = 1;
             break;
@@ -34,7 +52,5 @@ function getChannelColor(colorInHex, channelName) {
     const channelColorHex = colorInHex.substr(start, 2);
     const channelColorDec = parseInt(channelColorHex, 16);
 
-    return channelColorDec; 
+    return channelColorDec;
 }
-
-
