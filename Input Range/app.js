860815bd54bea46c11e545bce49032e7babf5091
Michał Adamczyk <michal.robert.adamczyk@gmail.com>
2020-02-03 13:48:19 +0100

    js done

--- Input Range/app.js
+++ Input Range/app.js
@@ -1,4 +1,21 @@
 const init = function() {
+    const defaultRangeValue = 40;
+    const spanEl = document.querySelector('span');
+    const rangeEl = document.querySelector('input');
+    rangeEl.addEventListener('mousemove', showValue);
+    rangeEl.addEventListener('change', showValue);
+    spanEl.innerText = defaultRangeValue;
+    rangeEl.value = defaultRangeValue;
 
+    function showValue(e) {
+        const isMouseMoveEvent = e.type === 'mousemove';
+        const isMouseLeftButtonPress = e.buttons === 1;
+        if (
+            isMouseMoveEvent && isMouseLeftButtonPress ||
+            !isMouseMoveEvent
+        ) {
+            spanEl.innerText = e.target.value;
+        }
+    }
 };
 document.addEventListener('DOMContentLoaded', init);
