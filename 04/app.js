d2845e6f13e95f7546cb3ee9496a98b29d804793
Michał Adamczyk <michal.robert.adamczyk@gmail.com>
2020-02-04 15:22:40 +0100

    ustawiłem pobieranie wartości i adeventListener, problemy: range działa nieprawidłowo, nie potrafie żeby funkcje działały z użyciem pobranych wartosci

--- 04/app.js
+++ 04/app.js
@@ -6,51 +6,68 @@ function init() {
     const colorEl = document.querySelector('[type="color"]');
     rangeEl.setAttribute('min', '0,0');
     rangeEl.setAttribute('max', '1,0');
-    // rangeEl.setAttribute('step', '1');
-
-    // ?? dalczego jak ustawiam value na 1 albo 0 to tam się znajduje suwak, a jak na inną wartość, to w połowie??
-    rangeEl.setAttribute('value', '0,1');
-    console.log(rangeEl);
-    console.log(colorEl);
-    // 3. tworzę potrzebne mi zmienne
 
     // 4. tworzę nasłuchwianie dla zmiennych 
 
-    // 4. tworzę nasłuchwianie dla zmiennych 
+
+
 
     const boxElement = document.querySelector('.box');
     setBoxShadow(boxElement, '#000000');
-}
 
-// Ta funkcja ustawia ??
-function setBoxShadow(element, colorInHex, opacity = 1) {
-    const colorInRGBA = `rgba(
+
+    // Ta funkcja ustawia ??
+    rangeEl.addEventListener('mousemove', setValue);
+
+    function setValue(e) {
+        const isMouseMoveEvent = e.type === 'mousemove';
+        const isMouseLeftButtonPress = e.buttons === 1;
+        if (
+            isMouseMoveEvent && isMouseLeftButtonPress ||
+            !isMouseMoveEvent
+        ) {
+            const opacity = e.target.value;
+            console.log(opacity);
+        }
+    }
+
+    colorEl.addEventListener('change', changeBGColor);
+
+    function changeBGColor(e) {
+        const body = document.body;
+        const color = e.target.value;
+        body.style.backgroundColor = color;
+    }
+
+    function setBoxShadow(element, colorInHex, opacity = 1) {
+        const colorInRGBA = `rgba(
         ${getChannelColor(colorInHex, 'red')}, 
         ${getChannelColor(colorInHex, 'green')}, 
         ${getChannelColor(colorInHex, 'blue')}, 
         ${opacity}
     )`;
 
-    element.style.boxShadow = `0 0 5px 5px ${colorInRGBA}`;
-}
-
-
-function getChannelColor(colorInHex, channelName) {
-    let start;
-    switch (channelName) {
-        case 'red':
-            start = 1;
-            break;
-        case 'green':
-            start = 3;
-            break;
-        case 'blue':
-            start = 5;
-            break;
+        element.style.boxShadow = `0 0 5px 5px ${colorInRGBA}`;
     }
 
-    const channelColorHex = colorInHex.substr(start, 2);
-    const channelColorDec = parseInt(channelColorHex, 16);
 
-    return channelColorDec;
+    function getChannelColor(colorInHex, channelName) {
+        let start;
+        switch (channelName) {
+            case 'red':
+                start = 1;
+                break;
+            case 'green':
+                start = 3;
+                break;
+            case 'blue':
+                start = 5;
+                break;
+        }
+
+        const channelColorHex = colorInHex.substr(start, 2);
+        const channelColorDec = parseInt(channelColorHex, 16);
+
+        return channelColorDec;
+    }
 }
